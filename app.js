f7a0bb47ea06f49e90dedca5a4d70f834de63211
Mike <mvalenti14@live.com>
2022-03-24 17:04:10 -0400

    Connect Mongoose to app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 //use static files in the folder named public
 app.use(express.static("public"));
 
-
+mongoose.connect("mongodb://localhost:27017/todoListDB");
 
 app.get("/", function(req, res) {
 
