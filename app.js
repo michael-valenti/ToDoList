a2ff9ff3173fc80aa0ba7f8f87d42055ff88eb40
Mike <mvalenti14@live.com>
2022-03-30 00:53:07 -0400

    Heroku deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const port = 3000;
+
 require('dotenv').config();
 const mongoose = require('mongoose');
 const date = require(__dirname + "/date.js");
